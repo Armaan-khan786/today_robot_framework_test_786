f9d556e7efd5f5bd5a221e7bf2cfd0e6d9d08eda
Armaan-khan786 <a75777129@gmail.com>
2026-02-11 14:08:24 +0530

    Update receiver1.ino

--- receiver1/receiver1.ino
+++ receiver1/receiver1.ino
@@ -1,32 +1,20 @@
 // Receiver ESP32 - UART2
 HardwareSerial MySerial(2); // UART2
 
-int messageCount = 0;
-bool started = true; // start receiving automatically
-
 void setup() {
-  Serial.begin(115200);                       // PC monitor
-  MySerial.begin(115200, SERIAL_8N1, 16, 17); // UART2: RX2=16, TX2=17
+  Serial.begin(115200);                    // PC monitor
+  MySerial.begin(115200, SERIAL_8N1, 16, 17); // RX2=16, TX2=17 from sender
   delay(2000);
   Serial.println("RECEIVER READY");
 }
 
 void loop() {
-  if (started && MySerial.available()) {
+  if(MySerial.available()){
     String msg = MySerial.readStringUntil('\n');
     msg.trim();
-
-    if (msg.length() > 0) {
-      messageCount++;
+    if(msg.length() > 0){
       Serial.print("RECEIVED: ");
-      Serial.println(msg);
-
-      if (msg == "DONE") {
-        Serial.print("TOTAL MESSAGES RECEIVED: ");
-        Serial.println(messageCount - 1); // exclude DONE
-        Serial.println("WAITING AGAIN...");
-        started = false; // stop after first batch
-      }
+      Serial.println(msg);  // Print received message on PC monitor
     }
   }
 }
