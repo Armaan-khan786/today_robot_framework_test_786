b2e7a6cb3dee1559973fe749a6142b29f9f32fd3
Armaan-khan786 <a75777129@gmail.com>
2026-02-11 13:39:20 +0530

    Update uart_library.py

--- uart_library.py
+++ uart_library.py
@@ -9,17 +9,24 @@ def open_ports(sender_port, receiver_port, baudrate=115200):
     sender = serial.Serial(sender_port, baudrate, timeout=2)
     receiver = serial.Serial(receiver_port, baudrate, timeout=2)
     time.sleep(2)
+    print("PORTS OPENED")
 
 def read_from_sender():
     global sender
     if sender and sender.in_waiting > 0:
-        return sender.readline().decode().strip()
+        msg = sender.readline().decode(errors='ignore').strip()
+        if msg != "":
+            print(f"SENDER SENT: {msg}")
+        return msg
     return ""
 
 def read_from_receiver():
     global receiver
     if receiver and receiver.in_waiting > 0:
-        return receiver.readline().decode().strip()
+        msg = receiver.readline().decode(errors='ignore').strip()
+        if msg != "":
+            print(f"RECEIVER GOT: {msg}")
+        return msg
     return ""
 
 def close_ports():
@@ -28,3 +35,4 @@ def close_ports():
         sender.close()
     if receiver:
         receiver.close()
+    print("PORTS CLOSED")
